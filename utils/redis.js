a22bdaee0e938fc3d48ff851aa01ba9422fd6fdf
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-12-19 21:06:44 +0200

    0 edit

--- utils/redis.js
+++ utils/redis.js
@@ -2,45 +2,46 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('connect', () => {
-            console.log('Redis client connected successfully.');
-        });
-        this.client.on('error', (err) => {
-            console.error('Error connecting to Redis:', err);
-        });
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('connect', () => {
+      console.log('Redis client connected successfully.');
+    });
+    this.client.on('error', (err) => {
+      console.error('Error connecting to Redis:', err);
+    });
 
-        // Promisify Redis methods
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.setAsync = promisify(this.client.set).bind(this.client);
-        this.delAsync = promisify(this.client.del).bind(this.client);
-    }
+    // Promisify Redis methods
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        if (typeof key !== 'string') {
-            throw new Error('Key must be a string value');
-        }
-        return await this.getAsync(key);
+  async get(key) {
+    if (typeof key !== 'string') {
+      throw new Error('Key must be a string value');
     }
+    const value = await this.getAsync(key);
+    return value;
+  }
 
-    async set(key, value, duration) {
-        if (typeof key !== 'string') {
-            throw new Error('Key must be a string value');
-        }
-        await this.setAsync(key, value, 'EX', duration);
+  async set(key, value, duration) {
+    if (typeof key !== 'string') {
+      throw new Error('Key must be a string value');
     }
+    await this.setAsync(key, value, 'EX', duration);
+  }
 
-    async del(key) {
-        if (typeof key !== 'string') {
-            throw new Error('Key must be a string value');
-        }
-        await this.delAsync(key);
+  async del(key) {
+    if (typeof key !== 'string') {
+      throw new Error('Key must be a string value');
     }
+    await this.delAsync(key);
+  }
 }
 
 const redisClient = new RedisClient();
